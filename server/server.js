f98fd176b5c8e26c82e2edc4a2b6b6910dec5443
nova <nova.corps1010@outlook.com>
2021-10-29 11:21:24 -0400

    Finish out routes

--- server/server.js
+++ server/server.js
@@ -2,22 +2,59 @@ require("dotenv").config();
 const express = require("express");
 
 const app = express();
-
 const port = process.env.PORT || 3001;
+
+// Defining the body property in the response
+app.use(express.json());
 
 // Building a restful API
 // DEFINING THE ROUTES
 
-app.get("/getRestaurants", (req, res) => {
-    res.json({
+app.get("/api/v1/restaurants", (req, res) => {
+
+    res.status(200).json({
         status: "success",
-        restaurant: "MCDonalds",
-        quality: "Delicious hamburguers"
+        data: {
+            restaurant: "MCDonalds"
+        }
+    });
+});
+
+
+app.get("/api/v1/restaurants/:id", (req, res) => {
+    console.log(req);
+});
+
+
+app.post("/api/v1/restaurants/", (req, res) => {
+    res.status(201).json({
+        status: "success",
+        data: {
+            restaurant: "MCDonalds"
+        }
     });
 });
 
 
 
+app.put("/api/v1/restaurants/:id", (req, res) => {
+    console.log(req.params.id);
+    console.log(req.body);
+
+    res.status(200).json({
+        status: "success",
+        data: {
+            restaurant: "MCDonalds"
+        }
+    });
+});
+
+
+app.delete("/api/v1/restaurants/:id", (req, res) => {
+    res.status(200).json({
+        status: "Deleted succesfully baby"
+    });
+});
 
 
 
