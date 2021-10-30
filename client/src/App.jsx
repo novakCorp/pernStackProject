c0bc38c10c4b5c2b1305080f912584e8ddb6931e
nova <nova.corps1010@outlook.com>
2021-10-29 22:30:58 -0400

    react router setup

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import UpdatePage from './routes/UpdatePage';
+import RestaurantDetailPage from './routes/RestaurantDetailPage';
+
+
+
+const App = () => {
+    return (
+
+        <div>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/restaurants/:id" component={RestaurantDetailPage} />
+                    <Route exact path="/restaurants/:id/update" component={UpdatePage} />
+                </Switch>
+            </Router>
+        </div>
+
+    )
+}
+
+export default App;
