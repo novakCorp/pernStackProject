20d0488a5d4a8562cd15cbc2f83ed84b87fb472e
nova <nova.corps1010@outlook.com>
2021-10-31 17:20:38 -0400

    React Context API

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,22 +3,24 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import UpdatePage from './routes/UpdatePage';
 import RestaurantDetailPage from './routes/RestaurantDetailPage';
-
+import { RestaurantsContextProvider } from './context/RestaurantContext';
 
 
 const App = () => {
     return (
+        <RestaurantsContextProvider>
 
-        <div className="container">
-            <Router>
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/restaurants/:id" component={RestaurantDetailPage} />
-                    <Route exact path="/restaurants/:id/update" component={UpdatePage} />
-                </Switch>
-            </Router>
-        </div>
+            <div className="container">
+                <Router>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/restaurants/:id" component={RestaurantDetailPage} />
+                        <Route exact path="/restaurants/:id/update" component={UpdatePage} />
+                    </Switch>
+                </Router>
+            </div>
 
+        </RestaurantsContextProvider>
     )
 }
 
