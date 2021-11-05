b9d894203e81a88244bc03e85baaad6a5b4ab9c7
nova <nova.corps1010@outlook.com>
2021-11-05 11:02:30 -0400

    updating restaurants

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -1,10 +1,14 @@
 import React, { useEffect, useContext } from 'react';
 import RestaurantFinder from '../apis/RestaurantFinder';
 import { RestaurantsContext } from '../context/RestaurantContext';
+import { useHistory } from 'react-router-dom';
 
 const RestaurantList = (props) => {
 
     const { restaurants, setRestaurants } = useContext(RestaurantsContext);
+
+    // represents the history of my browser
+    let history = useHistory();
 
     useEffect(() => {
         const fetchData = async () => {
@@ -42,6 +46,11 @@ const RestaurantList = (props) => {
         }
     }
 
+    const handleUpdate = (id) => {
+        // redirect to the page we're looking for
+        history.push(`/restaurants/${id}/update`);
+    }
+
     return (
 
         <div className="list-group">
@@ -65,7 +74,7 @@ const RestaurantList = (props) => {
                                     <td> {restaurant.location} </td>
                                     <td> {"$".repeat(restaurant.price_range)} </td>
                                     <td> reviews </td>
-                                    <td> <button className="btn btn-warning">Update</button> </td>
+                                    <td> <button onClick={() => handleUpdate(restaurant.id)} className="btn btn-warning">Update</button> </td>
                                     <td> <button onClick={() => handleDelete(restaurant.id)} className="btn btn-danger">Delete</button> </td>
                                 </tr>
                             );
